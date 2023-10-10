b5d47656dd3472447230427f07b551fc762af8e4
lokeshkoliparthy <147428633+lokeshkoliparthy@users.noreply.github.com>
2023-10-10 22:37:04 +0530

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Bluberry')
+streamlit.text('kale, Spinach & Rocket Smoothie')
+stremit.text('Boiled egg')
